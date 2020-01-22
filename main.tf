d61684a18cea9d35efd6ab9bf51b22929b5ba3e3
User <user@Student.local>
2020-01-22 15:42:03 +0900

    second commit

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+
 terraform {
-required_version = "~> 0.12"
+  required_version = "~> 0.12"
 }
+
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region = var.region
+}
+
+resource "aws_instance" "hello-tf-instance" {
+  ami = var.ami
+  count = var.hello_tf_instance_count
+  instance_type = var.hello_tf_instance_type
+  subnet_id = "subnet-04a85bd057b5fe49c"
+}
+
